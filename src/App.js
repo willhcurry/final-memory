e9e5552d293ec57c451081848cffe001887f580f
willhcurry <willhcurry@gmail.com>
2021-12-10 16:07:05 -0700

    Added card shuffling and turn initializations and save to state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/Bard.png" },
+  { "src": "/img/BlackMage.png" },
+  { "src": "/img/DarkKnight.png" },
+  { "src": "/img/Dragoon.png" },
+  { "src": "/img/GunBreaker.png" },
+  { "src": "/img/Monk.png" },
+  { "src": "/img/Ninja.png" },
+  { "src": "/img/Paladin.png" },
+  { "src": "/img/Samurai.png" },
+  { "src": "/img/Summoner.png" },
+  { "src": "/img/Warrior.png" },
+  { "src": "/img/WhiteMage.png" }
+]
+
 function App() {
+  //state of cards and turns
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //Shuffle cards
+  const shuffleCards = () => {
+    //duplicate cardImages twice(for matching) into a new array of cards
+    const shuffledCards = [...cardImages, ...cardImages]
+      //shuffle the new array of cards
+      .sort(() => Math.random() - 0.5)
+      //assign an id for each shuffled card
+      .map((card) => ({ ...card, id: Math.random }))
+
+    //initialize shuffled cards and turns
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Final Memory</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
